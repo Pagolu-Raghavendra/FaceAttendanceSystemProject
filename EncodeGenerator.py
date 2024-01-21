68ac1b92f62dad32d7d1fa04634c587d2da35327
Pagolu Raghavendra <114972757+Pagolu-Raghavendra@users.noreply.github.com>
2024-01-21 18:48:36 +0530

    Update EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -10,8 +10,8 @@ from firebase_admin import storage
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,
                               {
-                                  'databaseURL':"https://attendance-system-6e2e6-default-rtdb.firebaseio.com/",
-                                  "storageBucket":"attendance-system-6e2e6.appspot.com"
+                                  'databaseURL':"Google Realtime Database URL",
+                                  "storageBucket":"Google Bucket URL"
                               }
                               )
 
