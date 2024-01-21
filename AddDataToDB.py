2e5363fb7d2faa0f3b086bcd6a6b459ac3927a9d
Pagolu Raghavendra <114972757+Pagolu-Raghavendra@users.noreply.github.com>
2024-01-21 18:46:16 +0530

    Update AddDataToDB.py

--- AddDataToDB.py
+++ AddDataToDB.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,
                               {
-                                  'databaseURL':"https://attendance-system-6e2e6-default-rtdb.firebaseio.com/"
+                                  'databaseURL':"Google Bucket URL"
                               }
                               )
 ref=db.reference('Students')
